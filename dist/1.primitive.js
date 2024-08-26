4957c487d0200988ab6682b8e8c13350694b860f
kimjuyoung99 <jjassb404@naver.com>
2024-08-26 23:16:22 +0900

    첫 커밋 TS 셋팅

---
+++ dist/1.primitive.js
@@ -0,0 +1,5 @@
+"use strict";
+{
+    let name = "john";
+    console.log(name);
+}
---
+++ src/1.primitive.ts
@@ -0,0 +1,23 @@
+{
+    let name: string = "test";
+    //let으로 설정 헸으니 재할당 가능
+    name = "test1";
+    console.log(name);
+    let fullName: string = `Somya`;
+    console.log(fullName);
+
+    let age=25;
+    console.log(age);
+
+    let isDone = false;
+    console.log(isDone);
+
+    let name1: undefined =undefined;
+    let name2: null=null;
+    let name3:undefined|null;
+
+    let list = [1,2,3];
+    
+    
+    
+}
